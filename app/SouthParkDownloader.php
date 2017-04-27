4dfe1977fff4a074a740d5a83f7debd766725043
Christian Raue <christian.raue@gmail.com>
2017-04-28 00:01:57 +0200

    updated copyright year range

--- app/SouthParkDownloader.php
+++ app/SouthParkDownloader.php
@@ -14,7 +14,7 @@ require_once(__DIR__.'/vo/Season.php');
  * This file is part of the South Park Downloader package.
  *
  * @author Christian Raue <christian.raue@gmail.com>
- * @copyright 2011-2014 Christian Raue
+ * @copyright 2011-2017 Christian Raue
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPLv3 License
  */
 class SouthParkDownloader {
